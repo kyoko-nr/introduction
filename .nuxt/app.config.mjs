f63b3d84e3d6f398b5ab7b1f27e7ebd73ff0d62b
kyoko-nr <kyokokitagawa@me.com>
2023-07-08 17:17:31 +0900

    Nuxt3の新しいプロジェクトを作成

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/kyokokitagawa/GitRepositories/introduction/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
